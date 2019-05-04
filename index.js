4e460c1ffa69efdb7933404b77ff748502f4bb0f
Jobin Johny <jobinjohny@Jobins-MacBook-Air.local>
2019-05-04 09:37:22 -0400

    initial checkin

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port=process.env.PORT || 3010
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello Jobin123</h1>');
+});
+server.listen(port,() => {
+console.log(`Server running at port  `+port);
+});
