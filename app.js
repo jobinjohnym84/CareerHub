1234c327675952e6386a5c6b7df969b564df4ccb
Harikrishnan N <n.hari757@gmail.com>
2019-05-05 18:24:10 -0400

    Update app.js
    Updated mongo db connection

--- app.js
+++ app.js
@@ -25,17 +25,16 @@ app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
 app.use(express.static(__dirname + '/app/public'));
 
 // build mongo database connection url //
+ process.env.DB_HOST = process.env.DB_HOST || 'localhost'
+ process.env.DB_PORT = process.env.DB_PORT || 27017;
+ process.env.DB_NAME = process.env.DB_NAME || 'node-login';
 
-// process.env.DB_HOST = process.env.DB_HOST || 'localhost'
-// process.env.DB_PORT = process.env.DB_PORT || 27017;
-// process.env.DB_NAME = process.env.DB_NAME || 'node-login';
-
-// if (app.get('env') != 'live'){
-// 	process.env.DB_URL = 'mongodb://'+process.env.DB_HOST+':'+process.env.DB_PORT;
-// }	else {
+ if (app.get('env') != 'live'){
+ 	process.env.DB_URL = 'mongodb://'+process.env.DB_HOST+':'+process.env.DB_PORT;
+ }	else {
 // // prepend url with authentication credentials // 
-// 	process.env.DB_URL = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+process.env.DB_HOST+':'+process.env.DB_PORT;
-// }
+ 	process.env.DB_URL = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+process.env.DB_HOST+':'+process.env.DB_PORT;
+ }
 
 app.use(session({
 	secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
